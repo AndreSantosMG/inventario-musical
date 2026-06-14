eee0ba9660234f87eb957c87c57930770d72b563
AndreSantosMG <49284872+AndreSantosMG@users.noreply.github.com>
2026-06-14 15:45:10 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'inventario-v7';
+const CACHE_NAME = 'inventario-v8';
 const urlsToCache = [
   './', './index.html', './manifest.json', './css/style.css',
   './js/app.js', './js/db.js', './js/utils.js', './js/sync.js'
