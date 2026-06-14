fa84224a3574632f03dd3f71561cfed3e78ae643
AndreSantosMG <49284872+AndreSantosMG@users.noreply.github.com>
2026-06-14 17:47:57 -0300

    Atualizar o sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'inventario-v11';
+const CACHE_NAME = 'inventario-v12';
 const urlsToCache = [
   './', './index.html', './manifest.json', './css/style.css',
   './js/app.js', './js/db.js', './js/utils.js', './js/sync.js'
